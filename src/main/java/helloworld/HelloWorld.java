b1fc34cf9f5f6cdf02f196e0dabee4ba4eaca572
Michel Levy <mlevy5722@gmail.com>
2015-07-21 22:18:20 +0000

    hellow world #3

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String args[]){
 
-		System.out.println("Hello World, Jenkins -02 ");
+		System.out.println("Hello World, Jenkins -03 ");
 
 	}
 
