1c5a474ef3ba63995b4d505f2a8a8fd04e343214
Michel Levy <mlevy5722@gmail.com>
2015-07-22 16:00:28 +0000

    version 4

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String args[]){
 
-		System.out.println("Hello World, Jenkins -03 ");
+		System.out.println("Hello World, Jenkins -04 ");
 
 	}
 
